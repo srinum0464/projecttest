ea425ba8c377ca764ec2679a99af54dbe9a26343
srinivas <mallula.srinivas46@gmail.com>
2019-07-31 22:40:17 +0530

    added second python file

---
+++ second.py
@@ -0,0 +1,12 @@
+class ValueError(Exception):
+    def __init__(self,args):
+        self.me = args
+if __name__ == '__main__':
+    age = int(input('Enter age:'))
+    try:
+        if age < 18:
+            raise ValueError('Your not eligible to register vote minimum age is 18, you age is : {}'.format(age))
+        else:
+            print('You are eligible to register for vate')
+    except ValueError as e:
+            print(e)
