70bd21e5722cf9b13c8b3aade3ea5e7c74f00742
srinivas <mallula.srinivas46@gmail.com>
2019-07-31 21:41:34 +0530

    added python file

---
+++ first.py
@@ -0,0 +1,19 @@
+@pretiffy('=')
+def print_name(name):
+    print(name)
+
+@pretiffy('-')
+def print_city(city):
+    print(city)
+
+def pretiffy(symbol):
+    def decor(func):
+        def wrapper(arg):
+            print(symbol*len(arg))
+            func(arg)
+            print(symbol*len(arg))
+        return wrapper
+    return decor
+if __name__=='__main__':
+    print_city('Jakkaram')
+    print_name('Srinu')
