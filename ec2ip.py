188e69364938203ec9a17a4239db2dd02267ac2e
srinivas <mallula.srinivas46@gmail.com>
2019-08-25 09:14:06 +0530

    modified ec2ip file

--- ec2ip.py
+++ ec2ip.py
@@ -12,19 +12,20 @@ for instance in ec2.instances.all():
     if {'Key': 'Name', 'Value': 'Apache'} in instance.tags:
         if instance.private_ip_address not in ans_ip:
             ans_ip.append(instance.private_ip_address)
-            
+print(ans_ip)            
 
 with open('/home/ansible/hosts','ab') as file:
     for i in ans_ip:
-        if i not in ip:
-            file.write(i)
-            file.write('\r\n')
-            print('written ip:',i)
-            with open('/etc/ssh/ssh_config','ab') as ssh1:
-                ssh1.write("\rHost"+"\r "+i)
-                ssh1.write('\r\n')
-                ssh1.write('\r    StrictHostKeyChecking no')
-                ssh1.write('\r\n')
-                ssh1.write('\r    UserKnownHostsFile=/dev/null')
-                ssh1.write('\r\n')
-                print('written ssh file')
+        if i != None:
+            if i not in ip:
+                file.write(i)
+                file.write('\r\n')
+                print('written ip:',i)
+                with open('/etc/ssh/ssh_config','ab') as ssh1:
+                    ssh1.write("Host"+" "+i)
+                    ssh1.write('\r\n')
+                    ssh1.write('    StrictHostKeyChecking no')
+                    ssh1.write('\r\n')
+                    ssh1.write('    UserKnownHostsFile=/dev/null')
+                    ssh1.write('\r\n')
+                    
